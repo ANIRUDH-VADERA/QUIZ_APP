1ddf9a586305aebb636e10a9e9e90ce192e3c118
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2021-08-28 20:47:32 +0530

    Final Product

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ var next=document.querySelector(".next");
 var progress_fill=document.querySelector(".progress_fill");
 var loader=document.querySelector(".loader");
 var main=document.querySelector(".main");
-
+var time=document.querySelector(".time_main")
 
 
 var curr_ques={};
 var score=0;
 var questionCounter=0;
 let available_questions = [];
-
 
 function convert(alphabet)
 {
@@ -36,6 +35,7 @@ function convert(alphabet)
     }
 }
 
+
 fetch("https://task-3-api.herokuapp.com/questions")
     .then(res=>{
         return res.json();
@@ -52,6 +52,7 @@ fetch("https://task-3-api.herokuapp.com/questions")
             }
             return required_question;
         });
+        startTimer(10);
         startGame();
     })
     .catch(err=>{
@@ -67,9 +68,6 @@ function startGame()
     score=0;
     max=available_questions.length;
     progress_fill.style.width=((questionCounter+1)/max)*100+'%';
-    loader.classList.add("hidden");
-    main.classList.remove("hidden");
-    main.classList.add("main_hover")
     getNextQuestion();
 }
 
@@ -133,12 +131,7 @@ prev.addEventListener("click",function (){
 next.addEventListener("click",function (){
     if((questionCounter+1)==max)
     {
-        selected.map((answer,index)=>{
-            if(answer==available_questions[index].answer)
-            {
-                score++;
-            }
-        })
+        setScore();
         localStorage.setItem("mostRecentScore",score);
         localStorage.setItem("max",max);
         return window.location.assign("./results.html"); 
@@ -166,3 +159,35 @@ next.addEventListener("click",function (){
         getNextQuestion();
     }
 });
+
+function startTimer(timer)
+{
+    let timer_counter= timer*60;
+    setInterval(function (){
+        loader.classList.add("hidden");
+        main.classList.remove("hidden");
+        let minutes=Math.floor(timer_counter/60);
+        let seconds = timer_counter%60;
+        seconds=seconds<10 ? '0'+seconds : seconds;
+        minutes=minutes<10 ? '0'+minutes : minutes;
+        time.innerText=minutes+':'+seconds;
+        if(timer_counter==0)
+        {
+            setScore();
+            localStorage.setItem("mostRecentScore",score);
+            localStorage.setItem("max",max);
+            return window.location.assign("./results.html"); 
+        }
+        timer_counter--;
+    },1000)
+}
+
+function setScore()
+{
+    selected.map((answer,index)=>{
+        if(answer==available_questions[index].answer)
+        {
+            score++;
+        }
+    })
+}
